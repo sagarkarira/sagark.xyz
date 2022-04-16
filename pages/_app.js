333082644ca3a2a4c9c17168d86d71cf4675a37e
Sagar Karira <sagarkarira1992@gmail.com>
2022-04-16 19:36:37 +0530

    Added nprogress bar to show loading

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,13 @@ import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme';
 import { GlobalProvider } from '../components/globalState';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
   return (
